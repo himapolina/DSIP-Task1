da773236f3035a6b95481c787d5e16a447dd27e5
himapolina <67219127+himapolina@users.noreply.github.com>
2022-02-22 10:31:43 +0400

    commit on master

--- restaurant-web-page/script.js
+++ restaurant-web-page/script.js
@@ -1,9 +1,20 @@
-function Function() {
-    var x = document.getElementById("navbar");
-    
-    if (x.className === "topnav") {
-        x.className += "  responsive";
-    } else {
-        x.className = "topnav";
-    }
+var menuContainer = document.getElementsByClassName("menu-items");
+var items = menuContainer.getElementsByClassName("item");
+function highlight1(){
+    var curr = document.getElementsByClassName("active");
+    var newItem = document.getElementsByClassName("item1");
+    newItem.className+= " active";
+    curr.className.replace(" active","");
+}
+function highlight2(){
+    var curr = document.getElementsByClassName("active");
+    var newItem = document.getElementsByClassName("item2");
+    newItem.className+= " active";
+    curr.className.replace(" active","");
+}
+function highlight3(){
+    var curr = document.getElementsByClassName("active");
+    var newItem = document.getElementsByClassName("item3");
+    newItem.className+= " active";
+    curr.className.replace(" active","");
 }
